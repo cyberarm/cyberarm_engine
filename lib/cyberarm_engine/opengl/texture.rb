f662fabc56467ac9c8efe13617659f3fc981822e
Cyberarm <matthewlikesrobots@gmail.com>
2021-03-29 08:13:59 -0500

    Use GL_SRGB_ALPHA for textures

--- lib/cyberarm_engine/opengl/texture.rb
+++ lib/cyberarm_engine/opengl/texture.rb
@@ -54,7 +54,7 @@ module CyberarmEngine
       texture_id = tex_names_buf.unpack1("L2")
 
       glBindTexture(GL_TEXTURE_2D, texture_id)
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, array_of_pixels)
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_SRGB_ALPHA, image.width, image.height, 0, GL_RGBA, GL_UNSIGNED_BYTE, array_of_pixels)
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT)
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
       glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST) if @retro
